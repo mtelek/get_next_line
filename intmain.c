bde8e486041d3bb84ad0804fd865242ee01b9758
mtelek <145149229+mtelek@users.noreply.github.com>
2023-10-19 12:34:02 +0000

    withintmain

---
+++ intmain.c
@@ -0,0 +1,20 @@
+int main(void)
+{
+    int fd;
+    char *line;
+
+    fd = open("txtfile.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    while (get_next_line(fd, &line) > 0)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+}
